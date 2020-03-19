366c110282e5854659cd414f30e057dada492c07
CodetochangeMinds <2016ucs0017@iitjammu.ac.in>
2020-03-19 12:19:21 +0530

    use of react router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,39 @@
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
 
 class Header extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      isNavOpen: false
+    }
+  }
+
   render() {
     return(
-    <React.Fragment>
-      <Navbar dark color="primary">
-        <div className="container">
-            <NavbarBrand href="/">Food Ordering App</NavbarBrand>
-        </div>
+    <div>
+      <Navbar dark expand="md">
+          <div className="container">
+              <NavbarToggler onClick={this.toggleNav} />
+              <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+              <Collapse isOpen={this.state.isNavOpen} navbar>
+                  <Nav navbar>
+                  <NavItem>
+                      <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                  </NavItem>
+                  </Nav>
+              </Collapse>
+          </div>
       </Navbar>
       <Jumbotron>
            <div className="container">
@@ -20,7 +45,7 @@ class Header extends Component {
                </div>
            </div>
        </Jumbotron>
-    </React.Fragment>
+    </div>
     );
   }
 }
---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+    return(
+      <div className="container">
+        <h4>Welcome This is the Home Page!</h4>
+      </div>
+    );
+}
+
+export default Home;  
+
