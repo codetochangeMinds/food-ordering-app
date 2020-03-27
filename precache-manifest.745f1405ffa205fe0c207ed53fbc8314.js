35b50a98d6a464be4b484be6c761d5243956de4d
CodetochangeMinds <2016ucs0017@iitjammu.ac.in>
2020-03-27 18:21:17 +0530

    Updates

---
+++ precache-manifest.745f1405ffa205fe0c207ed53fbc8314.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/food-ordering-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4bacad240fedde64ac4a",
+    "url": "/food-ordering-app/static/css/main.6525c62c.chunk.css"
+  },
+  {
+    "revision": "c7bd2ccac0f24a7e8fb4",
+    "url": "/food-ordering-app/static/js/runtime~main.41bec04f.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/food-ordering-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7d518c9f575c0576a8e1",
+    "url": "/food-ordering-app/static/js/2.6f09198e.chunk.js"
+  },
+  {
+    "revision": "4bacad240fedde64ac4a",
+    "url": "/food-ordering-app/static/js/main.afba9491.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/food-ordering-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/food-ordering-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/food-ordering-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7d518c9f575c0576a8e1",
+    "url": "/food-ordering-app/static/css/2.b8fa1313.chunk.css"
+  },
+  {
+    "revision": "53e3eb4af218b21540803b7a0dde8a48",
+    "url": "/food-ordering-app/index.html"
+  }
+];
